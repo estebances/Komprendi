f9df480b830580af67e6cc3665a82cfa67c2c3b1
ces-esteban <esteban.cespedes@thinkupsoft.com>
2022-10-18 17:49:20 -0300

    firts approach design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { styled } from '@mui/material/styles';
+import SearchInput from "./components/SearchInput";
+import StepperResult from "./components/StepperResult";
+
+const View = styled('div')(({ theme }) => ({
+  display: "flex",
+  justifyContent: "center"
+}));
+const Grid = styled('div')(({ theme }) => ({
+  minWidth: "50%",
+  minHeight: "97vh",
+  boxShadow: "0 5px 8px 0 rgba(0, 0, 0, 0.3)",
+  display: "grid",
+  justifyContent: "center"
+}));
+const Title = styled('div')(({ theme }) => ({
+  marginTop: "50px",
+  display: "flex",
+  justifyContent: "center"
+}));
+const Search = styled('div')(({ theme }) => ({
+  display: "flex",
+  justifyContent: "center"
+}));
+const Stepper = styled('div')(({ theme }) => ({
+  display: "flex",
+  justifyContent: "flex-start"
+}));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <View>      
+      <Grid>
+        <Title>KOMPRENI</Title> 
+        <Search><SearchInput/></Search>
+        <Stepper><StepperResult/></Stepper>
+      </Grid>
+    </View>
   );
 }
 
